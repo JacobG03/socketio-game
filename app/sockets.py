3fa495e1459f0186d86fdfd948717a1a2d01432b
JacobG <jaqobek1995@gmail.com>
2021-08-03 13:13:09 +0200

    fix

--- app/sockets.py
+++ app/sockets.py
@@ -1,3 +1,9 @@
 from app import socketio
 from flask_socketio import emit
 
+
+@socketio.on('my event')                         
+def test_message(message):                        
+    print('works')
+    emit('my response', {'data': 'got it!'}, broadcast=True)      
+            
